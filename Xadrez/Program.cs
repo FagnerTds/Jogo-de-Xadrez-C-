a75a03850a340bae016700547aed7cc65797d726
Fagner Tranquilino <fagnertds@gmail.com>
2024-03-27 13:40:21 -0300

    Criada a classe posição xadrez

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -9,24 +9,10 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-
-                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
-
-
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-
-            }
-
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
 
 
         }
---
+++ Xadrez/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna=coluna;
+            Linha=linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(Coluna-'a',8-Linha);
+        }
+        public override string ToString()
+        {
+            return "" + Coluna+Linha;
+        }
+    }
+}
