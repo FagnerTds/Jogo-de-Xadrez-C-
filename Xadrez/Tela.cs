fea64e349d7b780d16a92679a764a6ac66ece4af
Fagner Tranquilino <fagnertds@gmail.com>
2024-03-27 14:44:30 -0300

    Melhorando a implementação do metodo ImprimirTabuleiro

--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -5,20 +5,50 @@ using System.Text;
 using System.Threading.Tasks;
 using System;
 
-namespace Xadrez {
-    internal class Tela {
+namespace Xadrez
+{
+    internal class Tela
+    {
 
-        public static void imprimirTabuleiro(Tabuleiro tab) {
-            for (int i = 0; i < tab.linhas; i++) {
-                for (int j = 0; j < tab.colunas; j++) {
-                   
-                    if (tab.peca(i,j)==null)
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                Console.Write(8-i +" ");
+                for (int j = 0; j < tab.colunas; j++)
+                {
+
+                    if (tab.peca(i, j)==null)
                         Console.Write("- ");
-                    else Console.Write(tab.peca(i, j) + " ");
+                    else
+                    {
+                        ImprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
+                    }
+                        
+
                 }
                 Console.WriteLine();
             }
-            
+            Console.Write("  A B C D E F G H");
+
+        }
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+
+            }
         }
     }
 }
