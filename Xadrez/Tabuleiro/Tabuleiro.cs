50e0c03758abf99971fbe03f2aef0c938bcdc9e6
Fagner Tranquilino <fagnertds@gmail.com>
2024-03-27 12:52:08 -0300

    tratamento de excecoes

--- Xadrez/Tabuleiro/Tabuleiro.cs
+++ Xadrez/Tabuleiro/Tabuleiro.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace tabuleiro {
     internal class Tabuleiro {
         public int linhas { get; set; }
@@ -18,10 +19,33 @@ namespace tabuleiro {
         public Peca peca(int linha, int coluna) {
             return pecas[linha, coluna];
         }
-        public void colocarPeca(Peca p, Posicao pos)
+        public Peca peca(Posicao posicao)
         {
-            pecas[pos.Linha,pos.Coluna] = p;
-            p.posicao = pos;
+            return pecas[posicao.Linha, posicao.Coluna];
+        }
+        public bool existePeca(Posicao posicao)
+        {
+            validarPosicao(posicao);
+            return peca(posicao) != null;
+        }
+        public void colocarPeca(Peca p, Posicao posicao)
+        {
+            if (existePeca(posicao))
+                throw new TabuleiroException("Já existe uma peça nessa posição");
+            pecas[posicao.Linha,posicao.Coluna] = p;
+            p.posicao = posicao;
+        }
+        public bool posicaoValida(Posicao posicao)
+        {
+            if(posicao.Linha < 0 || posicao.Linha>linhas || posicao.Coluna<0 || posicao.Coluna>colunas)
+                return false;
+            return true;
+        }
+
+        public void validarPosicao(Posicao posicao)
+        {
+            if (!posicaoValida(posicao))
+                throw new Exception("Posição inválida");    
         }
     }
 }
