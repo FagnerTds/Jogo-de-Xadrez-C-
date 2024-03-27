a0b1cb34c10b89c27e99413fb6ec2e95c0a123b9
Fagner Tranquilino <fagnertds@gmail.com>
2024-03-27 11:47:51 -0300

    Inserindo peças ao tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Threading.Channels;
+using xadrez;
 using tabuleiro;
 
 namespace Xadrez {
@@ -8,8 +8,14 @@ namespace Xadrez {
 
             Tabuleiro tab = new Tabuleiro(8, 8);
 
+            
+            tab.colocarPeca(new Torre(Cor.Preta,tab), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(Cor.Preta,tab), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+
             Tela.imprimirTabuleiro(tab);
+
         }
-        
+
     }
 }
---
+++ Xadrez/xadrez/Rei.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using tabuleiro;
+
+namespace xadrez
+{
+    internal class Rei : Peca
+    {
+        public Rei(Cor cor,Tabuleiro tab) : base(cor,tab) { }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+    
+}
